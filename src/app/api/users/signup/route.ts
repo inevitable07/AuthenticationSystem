b0d5c26671bda21a23258924e0ab24682b4828f7
Aashish Bhaskar <imaashishbhaskar@gmail.com>
2026-03-14 03:22:05 +0530

    Sign Error Fix

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -43,6 +43,7 @@ export async function POST(request: NextRequest) {
 
 
         } catch (error: any) {
+            console.log("SIGNUP ERROR:", error);
             return NextResponse.json({error: error.message},
                 {status: 500}
             );
